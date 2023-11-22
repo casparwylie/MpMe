fc56a88264a85985b244f0b284919b8579a1876a
Caspar Wylie <casparwylie@Caspars-MacBook-Pro.local>
2023-11-22 18:26:07 +0000

    Don't use OTHER in search, format all!

--- mpme.py
+++ mpme.py
@@ -1,44 +1,44 @@
 import abc
 import dataclasses
+import json
 import os
 import platform
 import shutil
-import json
-from datetime import timedelta
 import subprocess
 import sys
 import time
+from datetime import timedelta
 
-import yt_dlp
 import eyed3
-
+import yt_dlp
 
 #################
 ### CONSTANTS ###
 #################
 PLATFORM = platform.platform()
-MISC_SONG_FOLDER_NAME = 'OTHER'
-TMP_DOWNLOAD_DIR = 'downloads'
-AUDIO_FORMAT = 'mp3'
-IGNORE_DISKS = {'Macintosh HD'}
-MAC_VOLUMES_DIR = '/Volumes'
-LINUX_MOUNT_DIR = '/mnt'
-BACKUP_DIR = 'backup'
+MISC_ARTIST_NAME = "Other"
+TMP_DOWNLOAD_DIR = "downloads"
+AUDIO_FORMAT = "mp3"
+IGNORE_DISKS = {"Macintosh HD"}
+MAC_VOLUMES_DIR = "/Volumes"
+LINUX_MOUNT_DIR = "/mnt"
+BACKUP_DIR = "backup"
 RETRY_ATTEMPTS = 3
-MAC_PLATFORM_PART = 'macOS'
-LINUX_PLATFORM_PART = 'Linux'
-SONG_DELIM_CHAR = '~'
+MAC_PLATFORM_PART = "macOS"
+LINUX_PLATFORM_PART = "Linux"
+SONG_DELIM_CHAR = "~"
 RESET_DOWNLOADS_EACH_RUN = False
 
-UNSUPPORTED_OS_MSG = 'Windows? Fuck off'
+UNSUPPORTED_OS_MSG = "Windows? Fuck off"
 
 ###############
 ### HELPERS ###
 ###############
 
+
 class YTDLogger:
     def debug(self, msg):
-        if msg.startswith('[debug] '):
+        if msg.startswith("[debug] "):
             pass
         else:
             self.info(msg)
@@ -52,18 +52,21 @@ class YTDLogger:
     def error(self, msg):
         print(msg)
 
+
 def mprint(message):
-  print(f'\n{message}\n')
+    print(f"\n{message}\n")
 
 
 YDL_BASE_OPTS = {
-    'format': 'bestaudio/best',
-    'logger': YTDLogger(),
-    'postprocessors': [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
-        'preferredquality': '192',
-    }],
+    "format": "bestaudio/best",
+    "logger": YTDLogger(),
+    "postprocessors": [
+        {
+            "key": "FFmpegExtractAudio",
+            "preferredcodec": "mp3",
+            "preferredquality": "192",
+        }
+    ],
     # May fix 403s according to https://stackoverflow.com/questions/32104702/youtube-dl-library-and-error-403-forbidden-when-using-generated-direct-link-by
     # 'cachedir': False,
 }
@@ -73,276 +76,277 @@ YDL_BASE_OPTS = {
 ### HELPER CLASSES ###
 ######################
 
+
 @dataclasses.dataclass
 class Song:
-  name: str
-  artist: str
+    name: str
+    artist: str
 
-  @property
-  def search_term(self):
-    return f'{self.name} {self.artist}'
+    @property
+    def search_term(self):
+        return (
+            f"{self.name} {self.artist}"
+            if self.artist != MISC_ARTIST_NAME
+            else self.name
+        )
 
-  @property
-  def full_name(self):
-    return f'{self.name} {SONG_DELIM_CHAR} {self.artist}'
+    @property
+    def full_name(self):
+        return f"{self.name} {SONG_DELIM_CHAR} {self.artist}"
 
-  @property
-  def file_name(self):
-    return f'{self.full_name}.{AUDIO_FORMAT}'
+    @property
+    def file_name(self):
+        return f"{self.full_name}.{AUDIO_FORMAT}"
 
-  @classmethod
-  def from_string(cls, string):
-    return cls(*string.split(SONG_DELIM_CHAR))
+    @classmethod
+    def from_string(cls, string):
+        return cls(*string.split(SONG_DELIM_CHAR))
 
-  def __str__(self):
-    return f'<{self.artist}: {self.name}>'
+    def __str__(self):
+        return f"<{self.artist}: {self.name}>"
+
 
 class SongList:
+    def __init__(self):
+        self.songs = []
 
-  def __init__(self):
-    self.songs = []
+    def populate(self):
+        options = (
+            ("from file", self.populate_from_file),
+            ("from json", self.populate_from_json),
+            ("from URL", self.populate_from_url),
+            ("manual", self.populate_from_input),
+        )
 
-  def populate(self):
-    options = (
-      ('from file', self.populate_from_file),
-      ('from json', self.populate_from_json),
-      ('from URL', self.populate_from_url),
-      ('manual', self.populate_from_input))
+        display_choices = " ".join(
+            f"\n({index + 1}) {name}" for index, (name, _) in enumerate(options)
+        )
+        while True:
+            choice = input(f"Please choose an option: {display_choices}\n:")
+            try:
+                self.songs = [
+                    Song.from_string(line)
+                    for line in sorted(options[int(choice) - 1][1]())
+                    if line
+                ]
+                break
+            except (IndexError, ValueError):
+                pass
 
-    display_choices = ' '.join(
-        f'\n({index + 1}) {name}'
-        for index, (name,_) in enumerate(options))
-    while True:
-      choice = input(
-        f'Please choose an option: {display_choices}\n:')
-      try:
-        self.songs = [
-          Song.from_string(line)
-          for line in sorted(options[int(choice) - 1][1]())
-          if line
-        ]
-        break
-      except (IndexError, ValueError):
-        pass
+    def populate_from_file(self):
+        """Excepts a linear list of Title~Artist"""
+        with open(input("Enter file name path: ")) as data:
+            return data.read().split("\n")
 
-  def populate_from_file(self):
-    """Excepts a linear list of Title~Artist"""
-    with open(input('Enter file name path: ')) as data:
-      return data.read().split('\n')
+    def populate_from_json(self):
+        """Expects a JSON object of "Artist": ["Title 1", ...]"""
+        rows = []
+        with open(input("Enter file name path: ")) as data:
+            data = json.load(data)
+            for artist, titles in data.items():
+                for title in titles:
+                    rows.append(f"{title}{SONG_DELIM_CHAR}{artist}")
+            return rows
 
-  def populate_from_json(self):
-    """Expects a JSON object of "Artist": ["Title 1", ...]"""
-    rows = []
-    with open(input('Enter file name path: ')) as data:
-      data = json.load(data)
-      for artist, titles in data.items():
-        for title in titles:
-          rows.append(f"{title}{SONG_DELIM_CHAR}{artist}")
-      return rows
+    def populate_from_url(self):
+        raise Exception("Not implemented!")
 
-  def populate_from_url(self):
-    raise Exception('Not implemented!')
+    def populate_from_input(self):
+        mprint(
+            "Paste a list of song + artist names" "(Use ENTER then ctrl D when done): "
+        )
+        return sys.stdin.readlines()
 
-  def populate_from_input(self):
-    mprint(
-      'Paste a list of song + artist names'
-      '(Use ENTER then ctrl D when done): ')
-    return sys.stdin.readlines()
-
-  def __str__(self):
-    return '\n'.join(str(song) for song in self.songs)
+    def __str__(self):
+        return "\n".join(str(song) for song in self.songs)
 
 
 ############
 ### CORE ###
 ############
 
+
 class Fetcher:
+    def __init__(self, song_list):
+        self.song_list = song_list
+        self.failed = []
 
-  def __init__(self, song_list):
-    self.song_list = song_list
-    self.failed = []
+    def begin(self):
+        self.prepare()
+        self.fetch_all_songs()
 
-  def begin(self):
-    self.prepare()
-    self.fetch_all_songs()
+    def prepare(self):
+        if RESET_DOWNLOADS_EACH_RUN:
+            mprint("Clearing downloads...")
+            if os.path.exists(TMP_DOWNLOAD_DIR):
+                shutil.rmtree(TMP_DOWNLOAD_DIR)
+            os.mkdir(TMP_DOWNLOAD_DIR)
 
-  def prepare(self):
-    if RESET_DOWNLOADS_EACH_RUN:
-      mprint('Clearing downloads...')
-      if os.path.exists(TMP_DOWNLOAD_DIR):
-        shutil.rmtree(TMP_DOWNLOAD_DIR)
-      os.mkdir(TMP_DOWNLOAD_DIR)
+    def fetch_all_songs(self):
+        start = int(input("Start at index [0]: ") or 0)
+        total = len(self.song_list.songs)
+        average_download_seconds = 0
+        total_download_seconds = 0
+        eta_seconds = 0
+        for i, song in enumerate(self.song_list.songs[start:]):
+            eta_display = (
+                "eta {:0>8}".format(str(timedelta(seconds=eta_seconds)))
+                if eta_seconds
+                else "eta N/A"
+            )
+            real_index = i + start
+            mprint(f"[{real_index + 1}/{total} " f"({eta_display})] Fetching {song}...")
 
-  def fetch_all_songs(self):
-    start = int(input("Start at index [0]: ") or 0)
-    total = len(self.song_list.songs)
-    average_download_seconds = 0
-    total_download_seconds = 0
-    eta_seconds = 0
-    for i, song in enumerate(self.song_list.songs[start:]):
-      eta_display = (
-        'eta {:0>8}'.format(str(timedelta(seconds=eta_seconds)))
-        if eta_seconds else 'eta N/A'
-      )
-      real_index = i + start
-      mprint(
-        f'[{real_index + 1}/{total} '
-        f'({eta_display})] Fetching {song}...'
-      )
+            start_time = time.time()
+            if self.fetch_song(song):
+                self.tag_song(song)
+            end_time = time.time()
 
-      start_time = time.time()
-      if self.fetch_song(song):
-        self.tag_song(song)
-      end_time = time.time()
+            duration_seconds = end_time - start_time
+            total_download_seconds += duration_seconds
+            average_download_seconds = round(total_download_seconds / (i + 1), 2)
+            eta_seconds = average_download_seconds * (total - (real_index))
 
-      duration_seconds = end_time - start_time
-      total_download_seconds += duration_seconds
-      average_download_seconds = round(total_download_seconds / (i + 1), 2)
-      eta_seconds = average_download_seconds * (total - (real_index))
+        mprint("Done!")
+        mprint("Failed...")
+        for song in self.failed:
+            print(song.full_name)
+        print()
 
-    mprint('Done!')
-    mprint('Failed...')
-    for song in self.failed:
-      print(song.full_name)
-    print()
+    def fetch_song(self, song):
+        path = os.path.join(TMP_DOWNLOAD_DIR, song.full_name)
+        for attempt in range(RETRY_ATTEMPTS):
+            ydl_opts = YDL_BASE_OPTS | {
+                "outtmpl": f"{path}.%(ext)s",
+            }
+            try:
+                with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                    ydl.download([f"ytsearch:{song.search_term}"])
+                break
+            except Exception as error:
+                mprint(f"Issue: {error}")
+                print(f"Retrying {attempt + 1}/{RETRY_ATTEMPTS}")
+        else:
+            mprint("Unable to fetch song. Skipping...")
+            self.failed.append(song)
+            return
+        return True
 
-  def fetch_song(self, song):
-    path = os.path.join(TMP_DOWNLOAD_DIR, song.full_name)
-    for attempt in range(RETRY_ATTEMPTS):
-      ydl_opts = YDL_BASE_OPTS | {
-        'outtmpl': f'{path}.%(ext)s',
-      }
-      try:
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-          ydl.download([f'ytsearch:{song.search_term}'])
-        break
-      except Exception as error:
-        mprint(f'Issue: {error}')
-        print(f'Retrying {attempt + 1}/{RETRY_ATTEMPTS}')
-    else:
-      mprint('Unable to fetch song. Skipping...')
-      self.failed.append(song)
-      return
-    return True
-
-  def tag_song(self, song):
-    eyed3_file = eyed3.load(
-      os.path.join(TMP_DOWNLOAD_DIR, song.file_name)
-    )
-    eyed3_file.tag.artist = song.artist
-    eyed3_file.tag.save()
+    def tag_song(self, song):
+        eyed3_file = eyed3.load(os.path.join(TMP_DOWNLOAD_DIR, song.file_name))
+        eyed3_file.tag.artist = song.artist
+        eyed3_file.tag.save()
 
 
 class Exporter(abc.ABC):
-
-  @abc.abstractmethod
-  def export(self):
-    ...
+    @abc.abstractmethod
+    def export(self):
+        ...
 
 
 class ExternalDiskExporter(Exporter):
 
-  name = 'MP3 Player (external disk)'
+    name = "MP3 Player (external disk)"
 
-  def export(self):
-    disk_name = self.find_disks()
-    if LINUX_PLATFORM_PART in PLATFORM:
-      subprocess.check_call(['mount', f'/dev/{disk_name}', LINUX_MOUNT_DIR])
-      path = LINUX_MOUNT_DIR
-    elif MAC_PLATFORM_PART in PLATFORM:
-      path = os.path.join(MAC_VOLUMES_DIR, disk_name)
-    else:
-      raise Exception(UNSUPPORTED_OS_MSG)
-    print('Exporting songs to disk...')
-    shutil.copytree(TMP_DOWNLOAD_DIR, path, dirs_exist_ok=True)
+    def export(self):
+        disk_name = self.find_disks()
+        if LINUX_PLATFORM_PART in PLATFORM:
+            subprocess.check_call(["mount", f"/dev/{disk_name}", LINUX_MOUNT_DIR])
+            path = LINUX_MOUNT_DIR
+        elif MAC_PLATFORM_PART in PLATFORM:
+            path = os.path.join(MAC_VOLUMES_DIR, disk_name)
+        else:
+            raise Exception(UNSUPPORTED_OS_MSG)
+        print("Exporting songs to disk...")
+        shutil.copytree(TMP_DOWNLOAD_DIR, path, dirs_exist_ok=True)
 
-  def find_disks(self):
-    mprint('Searching for disks...')
-    while True:
-      if LINUX_PLATFORM_PART in PLATFORM:
-        available_disks = {disk for disk in os.listdir('/dev') if 'sd' in disk}
-      elif MAC_PLATFORM_PART in PLATFORM:
-        available_disks = set(os.listdir(MAC_VOLUMES_DIR))
-      else:
-        raise Exception(UNSUPPORTED_OS_MSG)
-      available_disks = list(available_disks - IGNORE_DISKS)
-      if not available_disks:
-        time.sleep(1)
-        continue
-      display_disks = '\n'.join(
-        f'({index + 1}) {disk}'
-        for index, disk in enumerate(available_disks))
-      try:
-        choice = input(f'Which disk?\n {display_disks}\n:')
-        return available_disks[int(choice) - 1]
-      except (IndexError, ValueError):
-        pass
+    def find_disks(self):
+        mprint("Searching for disks...")
+        while True:
+            if LINUX_PLATFORM_PART in PLATFORM:
+                available_disks = {disk for disk in os.listdir("/dev") if "sd" in disk}
+            elif MAC_PLATFORM_PART in PLATFORM:
+                available_disks = set(os.listdir(MAC_VOLUMES_DIR))
+            else:
+                raise Exception(UNSUPPORTED_OS_MSG)
+            available_disks = list(available_disks - IGNORE_DISKS)
+            if not available_disks:
+                time.sleep(1)
+                continue
+            display_disks = "\n".join(
+                f"({index + 1}) {disk}" for index, disk in enumerate(available_disks)
+            )
+            try:
+                choice = input(f"Which disk?\n {display_disks}\n:")
+                return available_disks[int(choice) - 1]
+            except (IndexError, ValueError):
+                pass
 
 
 class LocalExporter(Exporter):
 
-  name = 'Local (backup)'
+    name = "Local (backup)"
 
-  def export(self):
-    if not os.path.exists(BACKUP_DIR):
-      os.mkdir(BACKUP_DIR)
-    shutil.copytree(
-      TMP_DOWNLOAD_DIR, BACKUP_DIR, dirs_exist_ok=True)
+    def export(self):
+        if not os.path.exists(BACKUP_DIR):
+            os.mkdir(BACKUP_DIR)
+        shutil.copytree(TMP_DOWNLOAD_DIR, BACKUP_DIR, dirs_exist_ok=True)
 
 
 class GoogleDriveExporter(Exporter):
 
-  name = 'Google Drive'
+    name = "Google Drive"
 
-  def export(self):
-    raise Exception('Not implemented!')
+    def export(self):
+        raise Exception("Not implemented!")
 
 
 EXPORTERS = (
-  ExternalDiskExporter,
-  LocalExporter,
-  GoogleDriveExporter,
+    ExternalDiskExporter,
+    LocalExporter,
+    GoogleDriveExporter,
 )
 
 
 def offer_exports():
     for exporter in EXPORTERS:
-      export = (input(
-        f'Export to {exporter.name} [y]: ') or 'y') == 'y'
-      if export:
-        exporter().export()
-        mprint('Done!')
+        export = (input(f"Export to {exporter.name} [y]: ") or "y") == "y"
+        if export:
+            exporter().export()
+            mprint("Done!")
+
 
 ############
 ### MAIN ###
 ############
 
+
 def show_introduction():
-  print(
-    """
+    print(
+        """
 ** Welcome to MPME! **
 
 Scan, download and export music to an external source.
 
 Ready? Hit Enter!
-    """)
-  input()
+    """
+    )
+    input()
 
 
 def main():
-  show_introduction()
+    show_introduction()
 
-  song_list = SongList()
-  song_list.populate()
+    song_list = SongList()
+    song_list.populate()
 
-  fetcher = Fetcher(song_list)
-  fetcher.begin()
+    fetcher = Fetcher(song_list)
+    fetcher.begin()
 
-  offer_exports()
+    offer_exports()
+
+    mprint("Finished. Quitting.")
 
 
-  mprint('Finished. Quitting.')
-
-if __name__ == '__main__':
-  main()
+if __name__ == "__main__":
+    main()
